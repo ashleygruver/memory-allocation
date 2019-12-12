69a637e429bfc354c953630c6d870eb76191a8b4
rmxiao <48061594+rmxiao@users.noreply.github.com>
2019-12-12 13:49:35 -0500

    Update pa32.c

--- pa32.c
+++ pa32.c
@@ -40,27 +40,27 @@ addrs_t* VMalloc(size_t size)
 {
 	//TODO: Have things be put into the redirection table nicely (non-linear array traversal)
   
-  //Initiate timer, record padded bytes
+  	//Initiate timer, record padded bytes
 	unsigned necessaryPad;
 	if(size % 8)
 		necessaryPad = 8 - size % 8;
 	else
 		necessaryPad = 0;
   
-  unsigned long start, end;
+  	unsigned long start, end;
 	rdtsc(&start);
 
 	//We do not desire to allocate zero bytes
 	if (!size)
-  {
-    //Update HC info
-    rdtsc(&end);
+  	{
+    		//Update HC info
+    		rdtsc(&end);
 		mallocTime += end - start;
 		mallocs++;
 		fails++;
 
 		return NULL;
-  }
+  	}	
 
 	int index = 0;
 	while (!!redir_tabl[index] && index < 65536)
@@ -68,15 +68,15 @@ addrs_t* VMalloc(size_t size)
 
 	//If the table appears to be full, we return null.
 	if (index == 65536)
-  {
+  	{
 		//Update HC info
-    rdtsc(&end);
+    		rdtsc(&end);
 		mallocTime += end - start;
 		mallocs++;
 		fails++;
 
-    return NULL;
-  }
+    		return NULL;
+  	}
 
 	//Pad requested size to be a multiple of 8
 	if(!!(size % 8))
@@ -84,15 +84,15 @@ addrs_t* VMalloc(size_t size)
 	
 	//Can't go over that size limit!
 	if(*(addrs_t*)(baseptr + 8) + size > baseptr + *(unsigned*)(baseptr + 4))
-  {
+  	{
 		//Update HC info
-    rdtsc(&end);
+    		rdtsc(&end);
 		mallocTime += end - start;
 		mallocs++;
 		fails++;
     
-    return NULL;
-  }
+    		return NULL;
+  	}
 	
 	//Assuming that none of the bad stuff happened, we can allocate the appropriate space, and then update [freeptr] accordingly.
 	redir_tabl[index] = *(addrs_t*)(baseptr + 8);
@@ -101,11 +101,11 @@ addrs_t* VMalloc(size_t size)
 	//As we've added a new element, the size of things in the table grows by one.
 	*(unsigned*)(baseptr) += 1;
 
-  //Update HC info
-  rdtsc(&end);
+  	//Update HC info
+  	rdtsc(&end);
 	mallocTime += end - start;
 	mallocs++;
-  pad[index] = necessaryPad;
+  	pad[index] = necessaryPad;
 	padBytes += necessaryPad;
 
 	return &redir_tabl[index];
@@ -113,22 +113,34 @@ addrs_t* VMalloc(size_t size)
 
 void VFree(addrs_t* addr) //[addr] is like the index in the table
 {
-	//TO-DO: We're given an address outside of redir_tabl (or do we not assume this will occur?)
-  //Initiate timer
+  	//Initiate timer
 	unsigned long start, end;
 	rdtsc(&start);
 
 	//Can't free anything if there's nothing there
 	if(!*(unsigned*)(baseptr))
-  {
+  	{
 		//Update HC info
-    rdtsc(&end);
+    		rdtsc(&end);
 		freeTime += end - start;
 		frees++;
 		fails++;
 
-    return;
-  }
+    		return;
+  	}
+	
+	//Address out of bounds
+	if(!addr || addr > &redir_tabl[65535] || addr < &redir_tabl[0])
+	{
+		//Update HC info
+		rdtsc(&end);
+		freeTime += end - start;
+		frees++;
+		fails++;
+		
+		return;
+	}
+	
 	//Need to make something that gets all the addresses that need to be adjusted
 	int need_shifting[*(unsigned*)(baseptr)];
 
@@ -143,21 +155,21 @@ void VFree(addrs_t* addr) //[addr] is like the index in the table
 	int count = 0, index = 0, shifted = 0;
 	addrs_t curr;
 
-  int upper_index = *(unsigned*)(baseptr);
+  	int upper_index = *(unsigned*)(baseptr);
   
 	while(count < *(unsigned*)(baseptr) && index < 65536)
 	{
-    curr = redir_tabl[index];
+    		curr = redir_tabl[index];
 		if(curr)
 		{
 			if(curr > *addr) //Means [curr] needs to be adjusted
 			{
-        need_shifting[shifted++] = index;
+        			need_shifting[shifted++] = index;
 				if (!upper_bound || curr < upper_bound)
-        {
+        			{
 					upper_bound = curr;
-          upper_index = index;
-        }
+          				upper_index = index;
+        			}
 			}
 			count++;
 		}
@@ -172,7 +184,7 @@ void VFree(addrs_t* addr) //[addr] is like the index in the table
 		int block = 0;
 		while(block <= 65536 && need_shifting[block])
 			redir_tabl[need_shifting[block++]] -= size;
-      //This *should* move a block over per iteration
+      			//This *should* move a block over per iteration
 
 		*(addrs_t*)(baseptr + 8) -= size;
 	}
@@ -185,13 +197,13 @@ void VFree(addrs_t* addr) //[addr] is like the index in the table
 	//Block lost, decrement
 	*(unsigned*)(baseptr) -= 1;
 
-  //Update HC info
-  rdtsc(&end);
+  	//Update HC info
+  	rdtsc(&end);
 	freeTime += end - start;
 	frees++;
 
-  padBytes -= pad[upper_index - 1];
-  pad[upper_index - 1] = 0;
+  	padBytes -= pad[upper_index - 1];
+  	pad[upper_index - 1] = 0;
 }
 
 addrs_t* VPut(any_t data, size_t size)
@@ -205,10 +217,10 @@ addrs_t* VPut(any_t data, size_t size)
 void VGet(any_t return_data, addrs_t* addr, size_t size)
 {
 	if(addr)
-  {
-    memcpy(return_data, *addr, size);
-	  VFree(addr);
-  }
+  	{
+    		memcpy(return_data, *addr, size);
+	  	VFree(addr);
+  	}
 }
 
 void heapChecker()
@@ -216,18 +228,18 @@ void heapChecker()
 	unsigned allocBlocks = *(unsigned*)(baseptr);
 	unsigned allocBytes = *(addrs_t*)(baseptr + 8) - baseptr;
 	unsigned freeBlocks, freeBytes;
-  //Check if the allocated blocks occupy the whole space
-  if(allocBytes < *(unsigned*)(baseptr + 4))
-  {
-    //Discount data structures size (which is 16 bytes)
-    freeBytes = *(unsigned*)(baseptr + 4) - allocBytes - 16;
-    freeBlocks++;
-  }
-  //Discount data structures size
-  allocBytes -= 16;
+  	//Check if the allocated blocks occupy the whole space
+  	if(allocBytes < *(unsigned*)(baseptr + 4))
+  	{
+    		//Discount data structures size (which is 16 bytes)
+    		freeBytes = *(unsigned*)(baseptr + 4) - allocBytes - 16;
+    		freeBlocks++;
+ 	}
+  	//Discount data structures size
+  	allocBytes -= 16;
 
 	printf("--- HEAP CHECK ---\n");
-  printf("Number of allocated blocks : %d\n", allocBlocks);
+  	printf("Number of allocated blocks : %d\n", allocBlocks);
 	printf("Number of free blocks  : %d\n", freeBlocks);
 	printf("Raw total number of bytes allocated : %d\n", allocBytes - padBytes);
 	printf("Padded total number of bytes allocated : %d\n", allocBytes);
@@ -241,6 +253,5 @@ void heapChecker()
 	if(frees)
 		printf("Average clock cycles for a VFree request: %d\n", (unsigned)(freeTime / frees));
 	printf("Total clock cycles for all requests: %d\n", mallocTime + freeTime);
-
 }
 
